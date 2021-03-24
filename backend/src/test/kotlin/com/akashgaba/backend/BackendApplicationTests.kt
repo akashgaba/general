030bce30fd1cfedb515e1be509eaa47c286d0ac2
Akash Gaba <akashgaba@github.com>
2021-03-24 21:49:01 +0530

    Initial
    Signed-off-by: Akash Gaba <akashgaba@github.com>

---
+++ backend/src/test/kotlin/com/akashgaba/backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.akashgaba.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
